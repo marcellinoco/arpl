0b19a1440847f3f17b0ece82f171f2f018096169
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-06-16 07:53:01 +0700

    feat: add emails table

--- server/db/migration/000001_initial.up.sql
+++ server/db/migration/000001_initial.up.sql
@@ -1,4 +1,5 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE "users" (
     "uid" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "name" VARCHAR(255),
@@ -7,4 +8,25 @@ CREATE TABLE "users" (
     "accessToken" VARCHAR(255),
     "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "emails" (
+    "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "uid" UUID NOT NULL,
+
+    "date" DATE NOT NULL,
+    "from" VARCHAR(255) NOT NULL,
+    "subject" VARCHAR(255) NOT NULL,
+
+    "content" TEXT NOT NULL,
+    "summary" TEXT NOT NULL,
+    
+    "products" TEXT[] NOT NULL,
+    "priority" VARCHAR(20) NOT NULL,
+    "mood" VARCHAR(20) NOT NULL,
+    
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    FOREIGN KEY ("uid") REFERENCES "users" ("uid")
 );
